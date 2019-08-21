4793a15f1040aae3b5125784db2cdbd1de719253
Leonie Zorzi <Leonie.Zorzi@hotmail.com>
2019-08-21 16:47:47 +0100

    Adds index.js to controllers

--- src/tests/routes.tests.js
+++ src/tests/routes.tests.js
@@ -6,3 +6,15 @@ test("tape is working", t => {
   t.equal(1, 1, "one should be one");
   t.end();
 });
+
+test("Check / route is being serverd", t => {
+  request(app)
+    .get("/")
+    .expect(200)
+    .expect("content-type", /html/)
+    .end((err, res) => {
+      t.error(err);
+      t.equal(res.body, "hello world", "Should return hello world string");
+      t.end();
+    });
+});
