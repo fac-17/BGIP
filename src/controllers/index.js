43d9b1954d2facd2c733a50a61787fe06a423530
Leonie Zorzi <Leonie.Zorzi@hotmail.com>
2019-08-21 17:01:24 +0100

    Sets up home.js and index.js but commented out due to error

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,16 +1,16 @@
-const express = require("express");
-const path = require("path");
-const router = express.Router();
+// const express = require("express");
+// const path = require("path");
+// const router = express.Router();
 
-//import home route controller
-const home = require("./home");
-const quotes = require("./quotes");
-const error = require("./error");
+// //import home route controller
+// const home = require("./home");
+// const quotes = require("./quotes");
+// const error = require("./error");
 
-//add home route
-router.get("/", home.get);
-router.get("/quotes", quotes.get);
-router.use(error.client);
-router.use(error.server);
+// //add home route
+// router.get("/", home.get);
+// router.get("/quotes", quotes.get);
+// router.use(error.client);
+// router.use(error.server);
 
-module.exports = router;
+// module.exports = router;
