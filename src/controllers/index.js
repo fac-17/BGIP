91b61d8245d9140e90ef7fe05b3faa7f877228d8
Leonie Zorzi <Leonie.Zorzi@hotmail.com>
2019-08-22 13:08:20 +0100

    Sets up database, queries and post request

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,24 +1,23 @@
-
 const express = require("express");
 //const path = require("path");
 const router = express.Router();
 
 //import home route controller
 const home = require("./home");
-//const quotes = require("./quotes");
+const quotes = require("./quotes");
 
 //const error = require("./error");
 
 //add home route
 router.get("/", home.get);
-//router.get("/quotes", quotes.get);
-//router.use(error.client);
+// router.get("/:category", quotes.get);
+router.post("/submitquote", quotes.post);
+// router.use(error.client);
 //router.use(error.server);
 
 //From Reuben needs review
 // const express = require('express');
 // const router = express.Router();
-
 
 // //res.render('view', { title: 'my other page', layout: 'other' });
 
--- src/model/queries/postData.js
+++ src/model/queries/postData.js
@@ -1,9 +1,9 @@
 const db_connection = require("../db_connection");
 
-const postQuote = (category, quote, quotee, keyword, cb) => {
+const postQuote = (category, quote, author, keyword, cb) => {
   db_connection.query(
-    "INSERT INTO quotes(category, quote, quotee, keyword) VALUES ($1, $2, $3, $4)",
-    [category, quote, quotee, keyword],
+    "INSERT INTO quotes(category, quote, author, keyword) VALUES ($1, $2, $3, $4)",
+    [category, quote, author, keyword],
     (err, res) => {
       if (err) return cb(err);
       cb(null, res);
