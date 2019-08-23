837456b4bbcfc9e7776f0a98272bf66414451b8d
Emmanuel <emanagyei@gmail.com>
2019-08-23 11:56:44 +0100

    remove redundant const within quotes.js file >Relates #24

--- src/controllers/quotes.js
+++ src/controllers/quotes.js
@@ -1,24 +1,25 @@
-const url = require("url");
-const path = require("path");
 const getData = require("../model/queries/getData");
 const postData = require("../model/queries/postData");
-const express = require("express");
 
 exports.post = (req, response) => {
   const { category, quote, author, keyword } = req.body;
   postData(category, quote, author, keyword, (err, res) => {
     if (err) console.log(err);
-    else{
+    else {
       //redirect to endpoint with category as a query string
-      response.redirect(`/quotes/category/${category}`)
+      response.redirect(`/quotes/category/${category}`);
     }
   });
 };
 
-exports.getByCategory = (req,response) => {
-  let category=req.params.category;
+exports.getByCategory = (req, response) => {
+  let category = req.params.category;
   getData.getQuotes(category, (err, res) => {
     if (err) console.log(err);
-    response.render("quotes", { title: "quotes", cssPath: "/css/styleQuotes.css", categoryQuotes: res.rows });
+    response.render("quotes", {
+      title: "quotes",
+      cssPath: "/css/styleQuotes.css",
+      categoryQuotes: res.rows
+    });
   });
-}
+};
